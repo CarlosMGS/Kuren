e833dc16963065d711776f86efa1ee7e3a5c6710
AmaliaRegueira <34100871+AmaliaRegueira@users.noreply.github.com>
2020-04-10 12:15:09 +0200

    Update migration_etl.R

--- ETLs/migration_etl.R
+++ ETLs/migration_etl.R
@@ -1,7 +1,7 @@
 library(RODBC)
 library(odbc)
 
-#establecemos conexión
+#establecemos conexiÃ³n
 dbconnection <- odbcDriverConnect("driver={SQL Server Native Client 11.0};Server=localhost; Database=Mineria;Uid=; Pwd=; trusted_connection=yes")
 
 setwd("C:/Users/CGil/Documents/MIN")
@@ -25,8 +25,8 @@ data$Periodo <- substr(data$Periodo, 5,6)
 data
 
 
-#cargamos csv con la relación entre Comunidades y Provincias
-prov_ca <- read.csv2("list-pro.csv", sep="," , header = TRUE, encoding="UTF-8")
+#cargamos csv con la relaciÃ³n entre Comunidades y Provincias
+prov_ca <- read.csv2("list-pro.csv", sep=";" , header = TRUE, encoding="UTF-8")
 prov_ca <- prov_ca[-1]
 prov_ca <- prov_ca[-3]
 prov_ca <- prov_ca[-2]

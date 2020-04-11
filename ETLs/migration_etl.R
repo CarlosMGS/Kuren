91f36835df77799e983993dcbd80c4f1ca37a6d3
Carlos Moisés Gil Solanas <CGil@solidq.com>
2020-04-11 10:04:21 +0200

    prov-ca

--- ETLs/migration_etl.R
+++ ETLs/migration_etl.R
@@ -2,7 +2,7 @@ library(RODBC)
 library(odbc)
 
 #establecemos conexión
-dbconnection <- odbcDriverConnect("driver={SQL Server Native Client 11.0};Server=localhost; Database=Mineria;Uid=; Pwd=; trusted_connection=yes")
+dbconnection <- odbcDriverConnect("driver={SQL Server Native Client 11.0};Server=localhost; Database=MIN;Uid=; Pwd=; trusted_connection=yes")
 
 setwd("C:/Users/CGil/Documents/MIN")
 
@@ -26,9 +26,11 @@ data
 
 
 #cargamos csv con la relación entre Comunidades y Provincias
-prov_ca <- read.csv2("list-pro.csv", sep="," , header = TRUE, encoding="UTF-8")
+prov_ca <- read.csv2("list-pro.csv", sep=";" , header = TRUE, encoding="UTF-8")
 prov_ca <- prov_ca[-1]
 prov_ca <- prov_ca[-3]
+prov_ca <- prov_ca[-2]
+prov_ca <- prov_ca[-2]
 prov_ca <- prov_ca[-2]
 
 
@@ -37,7 +39,34 @@ colnames(prov_ca)[2] <- "Comunidades"
 
 prov_ca
 
+prov <- unique(data$Provincias)
+prov
+
+#conexion con la base de datos
+conn <- odbcDriverConnect('driver={SQL Server};server=DESKTOP-BM96OLK;database=MIN;trusted_connection=true')
+
+dim_prov <- sqlQuery(conn, "SELECT n_province FROM dbo.dim_provinces WHERE n_province <> 'Whole'")
 
 
 
+for( row in prov){
+  
+  if(!row %in% dim_prov$n_province){
+    
+    c_index <- match(row, prov_ca$Provincias)
+    
+    
+    insert_query <- paste("INSERT INTO dbo.dim_provinces (n_province, s_name)
+             VALUES ('",row,"','", prov_ca$Comunidades[c_index], "')", sep="")
+    
+    sqlQuery(conn, insert_query)
+  }
+  
+  
+}
+
+
+unique(prov_ca$Comunidades)
+
+data["2019T1"] <- new_vector
 

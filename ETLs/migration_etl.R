20cc9a112a65dc52f632cd067f888e0904417dc4
Carlos Moisés Gil Solanas <CGil@solidq.com>
2020-04-15 14:01:31 +0200

    ETL modificada

--- ETLs/migration_etl.R
+++ ETLs/migration_etl.R
@@ -2,11 +2,12 @@ library(RODBC)
 library(odbc)
 library(dplyr)
 library(pracma)
+library(rjson)
 
-#establecemos conexi�n
-con <- odbcDriverConnect("driver={SQL Server Native Client 11.0};Server=localhost; Database=Mineria;Uid=; Pwd=; trusted_connection=yes")
+#establecemos conexi?n
+con <- odbcDriverConnect("driver={SQL Server Native Client 11.0};Server=DESKTOP-BM96OLK ; Database=Mineria;Uid=; Pwd=; trusted_connection=yes")
 
-setwd("C:/Users/amali/Downloads")
+setwd("C:/Users/CGIL/Documents/MIN")
 
 data <- read.csv2("24448.csv", encoding = "UTF-8")
 
@@ -22,7 +23,9 @@ data$Edad[is.na(data$Edad)] = 1000
 data$Year <- substr(data$Periodo, 1,4) 
 data$Periodo <- substr(data$Periodo, 5,6) 
 
-#cargamos csv con la relaci�n entre Comunidades y Provincias
+data<- transform(data,Total <- as.integer(Total))
+
+#cargamos csv con la relaci?n entre Comunidades y Provincias
 prov_ca <- read.csv2("list-pro.csv", sep=";" , header = TRUE, encoding="UTF-8")
 prov_ca <- prov_ca[-1]
 prov_ca <- prov_ca[-3]
@@ -37,124 +40,200 @@ colnames(prov_ca)[2] <- "Comunidades"
 prov <- unique(data$Provincias)
 prov
 
-dim_prov <- sqlQuery(con, "SELECT n_province FROM dbo.dim_provinces WHERE n_province <> 'Whole'")
+#eliminamos filas donde sexo se diferencie
+data <- data[data$Sexo == "Ambos sexos",]
+unique(data$Sexo)
+
+data <- data[-2]
+
+
+data <- data[-3]
+
+data[, 3] <- as.numeric(as.character( data[, 3] ))
 
 
 
-for( row in prov){
+
+#para cada provincia guardamos la comunidad autónoma a la que pertenece
+for(i in 1:nrow(data)){
+  c_index <- match(data$Provincias[i], prov_ca$Provincias)
   
-  if(!row %in% dim_prov$n_province){
-    
-    c_index <- match(row, prov_ca$Provincias)
-    
-    
-    insert_query <- paste("INSERT INTO dbo.dim_provinces (n_province, s_name)
-             VALUES ('",row,"','", prov_ca$Comunidades[c_index], "')", sep="")
+  data$Provincias[i] <- as.character(prov_ca$Comunidades[c_index])
+}
+
+
+#agrupamos por años y comunidades paraa adaptar los datos
+data <- group_by(data, Provincias, Edad, Year) %>% summarise(sum <- sum(Total))
+
+colnames(data)[4] <- "Total"
+
+data$Total[is.na(data$Total)] <- 0
+data <- data[data$Edad < 1000,]
+
+unique(data$Edad)
+
+unique(data$Year)
+
+#tabla de migraciones lista, sufrirá modificaciones porque debemos guardar varios datos para cada fila
+
+
+
+
+#cargamos fuentes de dimensiones
+#empresas
+company <- read.csv2("302.csv", encoding = "UTF-8")
+
+
+
+#eliminamos el total y nos quedamos con aquellas filas con un tipo de empresa específico
+colnames(company)[2]<-"Condicion"
+colnames(company)[1]<-"Provincia"
+colnames(company)[3]<-"Year"
+
+company$Provincia <- gsub("[1-90]", "", company$Provincia)
+company$Provincia <- gsub(" ", "", company$Provincia)
+
+#eliminamos los totales
+company <- company[company$Condicion != "Total",]
+company <- company[company$Provincia != "TotalNacional",]
+
+#eliminamos los años anteriores a 2008
+
+company[, 3] <- as.numeric(as.character( company[, 3] ))
+company <- company[company$Year > 2007,]
+
+#agrupamos por comunidad
+for(i in 1:nrow(company)){
+  c_index <- match(company$Provincia[i], prov_ca$Provincias)
+  
+  company$Provincia[i] <- as.character(prov_ca$Comunidades[c_index])
+}
+
+company[, 4] <- as.numeric(as.character( company[, 4] ))
+company <- group_by(company, Provincia, Condicion, Year) %>% summarise(sum <- sum(Total))
+colnames(company)[4]<- "Total"
+
+#Añadimos a los hechos el id por cada tipo de empresa
+unique(company$Condicion)
+data <- data[rep(seq_len(nrow(data)), each=9),]
+data["id_c"] <- rep(1:9, 20976)
+
+data <- data [order(data[,1],data[,3],data[,5]), ]
+company <- company[order(company[,1],company[,3]),]
+
+for(i in 0:nrow(company)-1){
+  
+
+  for(j in 0:90){
+  
+    data[i*91+j+1,5] <- i+1
+  
+  }
+}
+
+company$Total <- as.integer(company$Total)
+#cargamos los datos de empresas en su correspondiente dimension en el DW
+dim_com <- sqlQuery(con, "SELECT * FROM dbo.dim_companies")
+
+for(i in 1:nrow(company)){
+    insert_query <- paste("INSERT INTO dbo.dim_companies (quantity, class)
+             VALUES ('", company[i,4], "','",company$Condicion[i],"')", sep="")
     
     sqlQuery(con, insert_query)
-  }
-  
+}
+
+
+
+#educacion
+education <- fromJSON(file="6347.json")
+
+
+#pobreza
+poverty <- fromJSON(file="10011.json")
+
+#creamos tres listas para las dimensiones
+provinces <- list()
+ages <- list()
+risks <- list()
+
+provinces <-NULL
+ages <-NULL
+risks <- NULL
+
+provinces[80] <- "aaa"
+ages[80] <- "aaa"
+risks[80] <- "aaa"
+
+typeof(ages)
+
+
+#rellenamos las dimensiones
+for(i in 1:80){
+  provinces[i] <- poverty[[i]][[5]][[1]][3]
+  ages[i] <- poverty[[i]][[5]][[2]][3]
+  risks[i] <- poverty[[i]][[5]][[3]][3]
   
 }
 
-# GENERO
-sexo <- unique(data$Sexo)
-dim_sex <- sqlQuery(con, "SELECT n_gender FROM dbo.dim_gender")
+#convertimos las listas a arrays para rellenar el df
+provinces <- unlist(provinces, use.names=FALSE)
+ages <- unlist(ages, use.names=FALSE)
+risks <- unlist(risks, use.names=FALSE)
 
-for(i in sexo){ 
+#creamos el dataframe
+values <- data.frame(
+  "province" = provinces,
+  "ages"= ages,
+  "risk"=risks,
+  "2018"=1:80,
+  "2017"=1:80,
+  "2016"=1:80,
+  "2015"=1:80,
+  "2014"=1:80,
+  "2013"=1:80,
+  "2012"=1:80,
+  "2011"=1:80,
+  "2010"=1:80,
+  "2009"=1:80,
+  "2008"=1:80
+)
+
+values
+
+#rellenamos los valores para los porcentajes
+for(i in 1:80){
   
-  if((i %in% dim_sex$n_gender == FALSE) || nrow(dim_sex) < 3){
-    insert_query <- paste("INSERT INTO dbo.dim_gender (n_gender) VALUES ('", i, "')", sep="")
-    sqlQuery(con, insert_query)
+  for(j in 1:11){
+    
+    values[i, j+3]<-poverty[[i]][[6]][[j]][5]
+    
   }
   
 }
 
-#Anyos
-dim_years <- sqlQuery(con, "SELECT n_year FROM dbo.dim_year WHERE y_period = 'F'")
+values
 
-years_levels <- 2008:2019
-years_levels
+colnames(values)
 
-level %in% dim_years$n_year
+values <- values[values$province != "Total Nacional",]
+values <- values[,-2]
 
-for(level in years_levels){
-  
-  if((level %in% dim_years$n_year) == FALSE || length(dim_years)==0){
-    
-    #Completo(Full)
-    insert_query <- paste("INSERT INTO dbo.dim_year (n_year, y_period)
-             VALUES ('", level, "','F')", sep="")
-    
-    sqlQuery(con, insert_query)
-    
-    #Semestre1
-    insert_query <- paste("INSERT INTO dbo.dim_year (n_year, y_period)
-             VALUES ('", level, "','S1')", sep="")
-    
-    sqlQuery(con, insert_query)
-    
-    #Semestre2
-    insert_query <- paste("INSERT INTO dbo.dim_year (n_year, y_period)
-             VALUES ('", level, "','S2')", sep="")
-    
-    sqlQuery(con, insert_query)
-    
-    #Trimestre1
-    insert_query <- paste("INSERT INTO dbo.dim_year (n_year, y_period)
-             VALUES ('", level, "','T1')", sep="")
-    
-    sqlQuery(con, insert_query)
-    
-    #Trimestre2
-    insert_query <- paste("INSERT INTO dbo.dim_year (n_year, y_period)
-             VALUES ('", level, "','T2')", sep="")
-    
-    sqlQuery(con, insert_query)
-    
-    #Trimestre3
-    insert_query <- paste("INSERT INTO dbo.dim_year (n_year, y_period)
-             VALUES ('", level, "','T3')", sep="")
-    
-    sqlQuery(con, insert_query)
+
+#transformamos las columnas de los años en una unica columna añadiendo mas filas
+
+values <- values[rep(seq_len(nrow(values)), each=11),]
+values["year"] <- rep(2008:2018, 76)
+values["prct"] <- 1:836
+
+for(i in 0:75){
+  for(j in 1:11){
+    values[i*11 + j,15] <- values[i*11+j,2+j]
   }
-  
 }
 
-dim_yearsS1 <- sqlQuery(con, "SELECT id_year, n_year, y_period FROM dbo.dim_year WHERE y_period = 'S1'" )
-dim_yearsS2 <- sqlQuery(con, "SELECT id_year, n_year, y_period FROM dbo.dim_year WHERE y_period = 'S2'" )
-dim_gender <- sqlQuery(con, "SELECT id_gender, n_gender FROM dbo.dim_gender")
-dim_prov <- sqlQuery(con, "SELECT id_province, s_name, n_province FROM dbo.dim_provinces")
-
-for (row in prov){
-  c_index <- match(row, dim_prov$n_province)
-  
-  data$Provincias <- gsub(row,dim_prov$id_province[c_index], data$Provincias)
-  
-}
-
-colnames(data)[1] <- "id_province"
-
-for (row in dim_gender$n_gender){
-  c_index <- match(row, dim_gender$n_gender)
-  
-  data$Sexo <- gsub(row,dim_gender$id_gender[c_index], data$Sexo)
-  
-}
-
-colnames(data)[2] <- "id_gender"
-colnames(data)[3] <- "nm_age"
-
-for (row in data$Year){
-  d_index <- match(row, data$Year)
-  if( strcmp(data$Periodo[d_index], "S1") ){
-    c_indexD <-  match(row, dim_yearsS1$n_year)
-    data$Year[d_index] <- dim_yearsS1$id_year[c_indexD]
-  }
-  else{
-    c_indexD <-  match(row, dim_yearsS2$n_year)
-    data$Year[d_index] <- dim_yearsS2$id_year[c_indexD]
-  }
+#eliminamos las columnas sobrantes
+for(i in 1:11){
+  values <- values[,-3]
 }
 
 

209b22fc79e733e27f32fa511ebfb9d2d46ddcfd
Daniel <dipdnl14@gmail.com>
2020-05-02 13:42:37 +0200

    Regresion 2 - Nas borraos y columnas

--- Models/regresion_2.R
+++ Models/regresion_2.R
@@ -11,8 +11,7 @@ gc()
 con <- odbcDriverConnect("driver={SQL Server Native Client 11.0};Server=min-serv.database.windows.net ; Database=Mineria;Uid=usuariomin; Pwd=dctaMineria5")
 
 #query para traer los datos de trabajo
-data <- sqlQuery(con, "select comunidad, n_year, flow, age, id_com, dim_companies.class as com_type, quantity, id_edu, edu_ach, dim_edu_achieved.perc as edu_perc,
-                             id_pov, dim_poverty.class as pov_type, dim_poverty.perc as pov_perc
+data <- sqlQuery(con, "select *
                       from facts_migration left join dim_companies on (facts_migration.id_com = dim_companies.id_c)
                                            left join dim_edu_achieved on (facts_migration.id_edu = dim_edu_achieved.id_e)
 					                                 left join dim_poverty on (facts_migration.id_pov = dim_poverty.id_p)")
@@ -20,7 +19,22 @@ data <- sqlQuery(con, "select comunidad, n_year, flow, age, id_com, dim_companie
 mig <- data
 
 #eliminamos ids
+mig <- mig[,-1]
 mig <- mig[,-5]
-mig <- mig[,-7]
-mig <- mig[,-9]
+mig <- mig[,-5]
+mig <- mig[,-5]
+mig <- mig[,-5]
+mig <- mig[,-22]
+mig <- mig[,-14]
+
+#cambiamos los NA por -1 para que funcione 
+for(i in 1:length(mig)){
+  mig[is.na(mig[,i]), i] <- -1
+}
+
+cor(mig$flow, mig$car_material)
+
+andalusia30 <- mig[mig$comunidad=="Andalucía" && mig$age == 30, 3]
+
+plot(cpi, xaxt="n", ylab="CPI", xlab="")
 
